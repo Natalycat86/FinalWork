58ac526735aa7dfc2b600099555c7ef94b6c90cd
Natalia <natasha.litvina@mail.ru>
2022-12-24 09:55:26 +0300

    финальная работа

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+int NewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) NewArray++;
+}
+string[] NewArray = new string[NewArray];
+int count = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        NewArray[count] = array[i];
+        count++;
+    }
+}
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(NewArray);
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}

81cb4f69738e17326d727add23d2eb3bc710f951
Natalia <natasha.litvina@mail.ru>
2022-12-26 23:34:03 +0300

    изменения

--- Program.cs
+++ Program.cs
@@ -4,21 +4,21 @@ for (int i = 0; i <= array.Length - 1; i++)
 {
     if (array[i].Length <= 3) NewArray++;
 }
-string[] NewArray = new string[NewArray];
+string[] NewArray1 = new string[NewArray];
 int count = 0;
-
+ 
 for (int i = 0; i <= array.Length - 1; i++)
 {
     if (array[i].Length <= 3)
     {
-        NewArray[count] = array[i];
+        NewArray1[count] = array[i];
         count++;
     }
 }
 PrintArray(array);
 Console.Write(" -> ");
-PrintArray(NewArray);
-
+PrintArray(NewArray1);
+ 
 void PrintArray(string[] array)
 {
     Console.Write("[ ");
@@ -27,4 +27,4 @@ void PrintArray(string[] array)
         Console.Write($"“{array[i]}”, ");
     }
     Console.Write("] ");
-}
+} 
